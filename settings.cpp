915a170e9b03a5bccc7eaf3845e8f54f01d9837f
Marc Clint Dion <marcclintdion@Marcs-iMac.local>
2015-09-26 05:36:38 -0700

    init

---
+++ settings.cpp
@@ -0,0 +1,11 @@
+var         jadeMonkey_LIGHT_POSITION_01[]      =  {43.2259, 52.5437, 26.15, 1};
+
+var         jadeMonkey_powAdjust        =  4.93002;
+var         jadeMonkey_minAdjust        =  0.66;
+var         jadeMonkey_divisorAdjust        =  -5.76004;
+var           eyeposition[]      =  {0, 1.6, 17.4499, 0.0};
+var           lookAt[]        =  {0, -11.95, -42.3501, 0.0};
+var           rotateModelWithLeftMouse[]       =  {0.6, 373.502};
+
+var           eyeposition_SHADOW[]       =  {4.75, 3.65, 5.7, 1.0};
+var           eyeposition_SHADOW[]       =  {4.75, 3.65, 5.7}; 
